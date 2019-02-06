12a297876ca7375baedf1e3130b56d053d84b438
Alex1182-St <alexander.stepaniuk@yahoo.com.ua>
2019-02-06 17:59:34 +0200

    06/02/2019

--- src/main/java/Lesson3/BeginnerTask1.java
+++ src/main/java/Lesson3/BeginnerTask1.java
@@ -1,45 +1,20 @@
 package Lesson3;
 
-/*
-Given an array of integer numbers. Create a method (program) which returns a new one where
-each element is multiplied by 3
- */
-
 public class BeginnerTask1 {
-
-    int[] IntegerNumbers;
-
-    public int[] getIntegerNumbers() {
-        return IntegerNumbers;
-    }
-
-    public void setIntegerNumbers(int[] integerNumbers) {
-        IntegerNumbers = integerNumbers;
-    }
-
-    public BeginnerTask1(int[] integerNumbers) {
-        IntegerNumbers = integerNumbers;
-    }
-
 
     public static void main(String[] args) {
 
-        /*
-                BeginnerTask1 Task1 = new BeginnerTask1(int[] IntegerNumbers = {1, 5, 10, 50, 100, 0});
-                for (int i = 0; i < Task1.IntegerNumbers.length; i++) {
-                System.out.println(Task1.IntegerNumbers[i*3]);
+        System.out.println("Given an array of integer numbers.\n" +
+                "Create a method (program) which returns a new one where each element is multiplied by 3");
 
-                Можно ли так ??????
+        int[] array1 = {1, 25, 100, 400,0};
 
-                Я создаю новый обьект Task1, у которого переменная масив и во время создания указываю какие интовые значения в масиве??????
-         */
+        int[] ResultMult = new int[5];
 
-
-        int[] IntegerNumbers = {1,5,10,50,100,0};
-        for (int i = 0; i < IntegerNumbers.length; i++) {
-            System.out.println(IntegerNumbers[i*3]);
+        for(int i=0 ; i<array1.length ; i++) {
+            ResultMult[i] = array1[i]*3;
         }
-    }
-}
+        for (int i = 0; i < ResultMult.length; i++) {
+            System.out.println(ResultMult[i]);
 
-
+        }}}
